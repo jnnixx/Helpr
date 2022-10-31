b48e46c385c85d01b87ec9f8c5bbfef0ff06f93e
jnnixx <jennifer.fernanda699@gmail.com>
2022-10-31 11:56:35 -0300

    estudos de spring

---
+++ src/test/java/org/soulcodeacademy/Helpr/HelprApplicationTests.java
@@ -0,0 +1,13 @@
+package org.soulcodeacademy.Helpr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
